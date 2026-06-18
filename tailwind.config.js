57dabbdb954c80c5f67f0155a5903076484c84fa
shishant yadav <shishantyadav9636@gmail.com>
2026-06-18 23:00:50 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ["./src/**/*.{html,js}"],
+  safelist: [
+    "bg-gray-900",
+    "text-white",
+    "text-blue-400",
+    "text-4xl",
+    "font-bold",
+    "flex",
+    "items-center",
+    "justify-center",
+    "min-h-screen"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
